1cbe18ad053b8cf20a051520a7f2e459b0cc38ac
Kondwani Hara <kondwa@gmail.com>
2021-09-03 17:02:59 +0200

    Update init.php

--- init.php
+++ init.php
@@ -1,7 +1,14 @@
 <?php
 include "oi/oi.php";
 try{
-  $db->exec("CREATE TABLE IF NOT EXISTS mydb.ua(id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(64),lastname VARCHAR(64),email VARCHAR(64),password VARCHAR(300));");
+  $db->exec("CREATE TABLE IF NOT EXISTS mydb.ua(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    firstname VARCHAR(64),
+    lastname VARCHAR(64),
+    email VARCHAR(64),
+    password VARCHAR(300)
+    );"
+  );
   $db->exec("CREATE TABLE IF NOT EXISTS mydb.cash(
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     amount DECIMAL(13,2),
